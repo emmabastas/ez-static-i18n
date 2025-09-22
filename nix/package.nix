2219f0a030a4e2fe326d9578ba422fc8f39ecb14
Emma Bastås <emma.bastas@protonmail.com>
2025-09-22 07:46:35 +0200

    Bump patch

--- nix/package.nix
+++ nix/package.nix
@@ -6,27 +6,39 @@
 
 buildNpmPackage (finalAttrs: {
   pname = "ez-static-i18n-server";
-  version = "0.1.0";
+  version = "0.1.1";
 
   src = fetchFromGitHub {
     owner = "emmabastas";
     repo = "ez-static-i18n";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-BR+ZGkBBfd0dSQqAvujsbgsEPFYw/ThrylxUbOksYxM=";
+    hash = "sha256-q6cFRpB/u9r+f4gX+zaHd2yuE0SigtEmW6KtfwFK+RA=";
   };
 
-  npmDepsHash = "sha256-tuEfyePwlOy2/mOPdXbqJskO6IowvAP4DWg8xSZwbJw=";
+  npmDepsHash = "sha256-Ne18hDkk7jg4kqjhke2EsCCsgt6xwZb0s4Q7f7kyNg0=";
 
   npmPackFlags = [ "--ignore-scripts" ];
 
   npmBuildScript = "views:build";
+
+  forceGitDeps = true;
+
+  makeCacheWriteable = true;
 
   NODE_OPTIONS = "--experimental-strip-types";
 
   meta = {
     description = "Let non-technical people contribute to i18n for static websites.";
     homepage = "https://github.com/emmabastas/ez-static-i18n#readme";
-    license = lib.licenses.agpl3OrLater;
-    maintainers = with lib.maintainers; [ emmabastas ];
+    license = lib.licenses.agpl3Plus;
+    maintainers = with lib.maintainers; [
+      {
+        email = "emma.bastas@protonmail.com";
+        matrix = "@emmabastas:matrix.org";
+        github = "emmabastas";
+        githubId = 22533224;
+        name = "Emma Bastås";
+      }
+    ];
   };
 })
